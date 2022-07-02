1e7439e6f011c38a237a454f770b3625d3cda227
Darpan <darpan.madlani@gmail.com>
2022-07-02 20:11:36 +0530

    Emp wage calculated based on attendance

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,23 @@
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		final int PRESENT = 1;
+		final int FULL_TIME = 8;
+		final int WAGE_PER_HOUR = 20;	
+		int isEmpPresent = (int)(Math.random() * 2);
+		int empHr;
+	
+		//System.out.println(isEmpPresent);
+		
+		if (isEmpPresent == PRESENT) {
+			System.out.println("Employee is present");
+			empHr = FULL_TIME;
+		} else {
+			System.out.println("Employee is absent");
+			empHr = 0;
+		}
+		int empWage = empHr * WAGE_PER_HOUR;
+		System.out.println("Employee Wage : $"+emp+" USD");
+	}
+	
+}
