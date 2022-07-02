19b28e6883fba408e0ab0d928cb4f99a1ee33fa8
Darpan <darpan.madlani@gmail.com>
2022-07-02 20:26:18 +0530

    Part time wage calculation is added

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,22 +2,26 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		final int PRESENT = 1;
+		final int PART_TIME = 2;
 		final int FULL_TIME = 8;
 		final int WAGE_PER_HOUR = 20;	
-		int isEmpPresent = (int)(Math.random() * 2);
+		int isEmpPresent = (int)(Math.random() * 3);
 		int empHr;
 	
-		//System.out.println(isEmpPresent);
+		System.out.println(isEmpPresent);
 		
 		if (isEmpPresent == PRESENT) {
 			System.out.println("Employee is present");
 			empHr = FULL_TIME;
+		} else if(isEmpPresent == PART_TIME) {
+			System.out.println("Employee is working part-time");
+			empHr = FULL_TIME/2;
 		} else {
 			System.out.println("Employee is absent");
 			empHr = 0;
 		}
 		int empWage = empHr * WAGE_PER_HOUR;
-		System.out.println("Employee Wage : $"+emp+" USD");
+		System.out.println("Employee Wage : $"+empWage+" USD");
 	}
 	
 }
